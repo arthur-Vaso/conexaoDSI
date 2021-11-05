7775038ed4162aa4d317ed8ff381405d82a9b638
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-11-04 21:02:17 -0300

    Remoção de funções desnecessárias para que execute apenas a chamada a TelaPrincipal

--- PrimeiraConexao/src/br/edu/ifsp/principal/ExecutarProjeto.java
+++ PrimeiraConexao/src/br/edu/ifsp/principal/ExecutarProjeto.java
@@ -1,16 +1,12 @@
 package br.edu.ifsp.principal;
 
-import br.edu.ifsp.controlador.InserePessoaController;
-import br.edu.ifsp.tela.TelaPessoa;
-//import br.edu.ifsp.tela.TelaPrincipal;
+import br.edu.ifsp.tela.TelaPrincipal;
 
 public class ExecutarProjeto {
 
 	public static void main(String[] args) {
 		
-		//TelaPrincipal telaPrincipal = new TelaPrincipal();
-		TelaPessoa telaPessoa = new TelaPessoa("Formulario Pessoa");
-		InserePessoaController controller = new InserePessoaController(telaPessoa);
-		telaPessoa.setVisible(true);
+		TelaPrincipal telaPrincipal = new TelaPrincipal();
+		
 	}
 }
